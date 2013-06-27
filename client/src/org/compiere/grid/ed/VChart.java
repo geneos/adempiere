b9f4434290f483203f8aabb4bb9fbd75a17a4e46
tobi42 <none@none>
2013-06-27 18:42:05 +0200

    adding/updating license header
    --HG--
    branch : feature/ADEMPIERE-142-Sales-Management

--- client/src/org/compiere/grid/ed/VChart.java
+++ client/src/org/compiere/grid/ed/VChart.java
@@ -1,3 +1,22 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) <Company or Author Name> All Rights Reserved.                *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 or later of the GNU General Public License       *
+ * as published by the Free Software Foundation.                              *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *                                                                            *
+ * @author Adaxa Pty Ltd (Paul Bowden)                                        *
+ * FR [ 142 ] Sales Management (CRM)                                          *
+ * https://adempiere.atlassian.net/browse/ADEMPIERE-142                       *
+ *  			                                                              *
+ *****************************************************************************/
 package org.compiere.grid.ed;
 
 
@@ -22,12 +41,9 @@ import org.jfree.chart.entity.CategoryItemEntity;
 import org.jfree.chart.entity.ChartEntity;
 import org.jfree.chart.entity.PieSectionEntity;
 import org.jfree.chart.entity.XYItemEntity;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.general.DatasetGroup;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.time.TimeSeriesDataItem;
-import org.jfree.data.xy.XYDataset;
 
 public class VChart extends CPanel implements ChartMouseListener, VEditor {
 	
